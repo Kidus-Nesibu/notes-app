9c3a4e1f9256b92d5968182fdc3c4faedcc34368
Kidus Nesibu <knesibu3@gmail.com>
2026-07-31 14:58:12 +0300

    prepare app for deployment

--- notes_main/settings.py
+++ notes_main/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*1x9edfrz@ln0p!l9*c6ns=08i^8lrlgne7^m%#v_!b*sa@uc@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -116,3 +116,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
